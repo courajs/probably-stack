6aaa5dde497d25adef6723e7f45296d21f4d78ad
Aaron Sikes <aaron@sikes.io>
2017-05-01 20:02:29 -0400

    temp

--- app/router.js
+++ app/router.js
@@ -1,15 +1,19 @@
 import Ember from 'ember';
 import config from './config/environment';
 
+import { cardstackRoutes } from '@cardstack/routing';
+
 const Router = Ember.Router.extend({
   location: config.locationType,
   rootURL: config.rootURL
 });
 
 Router.map(function() {
-  this.route('streams', function() {
+  this.route('episodes', function() {
     this.route('show', { path: ':slug' });
   });
+
+  cardstackRoutes.call(this);
 });
 
 export default Router;
